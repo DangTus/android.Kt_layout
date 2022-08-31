594b407e410bddb63c601da4fbdbbd7239bbc2ca
DangTus <dangtu9520@gmail.com>
2022-09-01 00:10:16 +0700

    xong giao dien 2

--- app/src/main/java/com/dmt/dangtus/lt_ui/MainActivity.java
+++ app/src/main/java/com/dmt/dangtus/lt_ui/MainActivity.java
@@ -2,13 +2,32 @@ package com.dmt.dangtus.lt_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        anhXa();
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity2.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void anhXa() {
+        btnSignUp = (Button) findViewById(R.id.signUpButton);
     }
 }
